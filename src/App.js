805693ff30b50fc4e45db18cec6cb4723579b143
oliverlangmo <oliver.langmo@gmail.com>
2016-12-27 15:39:25 -0600

    adds and fetches json data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
 import './App.css';
 import Header from './Components/Header';
 import Contact from './Components/Contact';
@@ -10,7 +11,32 @@ import Testimonials from './Components/Testimonials';
 
 
 class App extends Component {
+constructor(props){
+   super(props);
+this.state= {
+foo: 'bar',
+resumeData:{}
+ };
+}
+getResumeData(){
+$.ajax({
+url:'http://localhost:3000/resumeData.json',
+dataType: 'json',
+cache: false,
+success: function(data){
+this.setState({resumeData: data});
+}.bind(this),
+error: function(xhr, status, err){
+console.log(err);
+alert(err);
+}
+});
+}
+componentDidMount(){
+this.getResumeData();
+}
   render() {
+console.log(this.state.resumeData);
     return (
       <div className="App">
         <Header />
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -26,7 +26,7 @@ class Header extends Component {
             <div className="banner-text">
                <h1 className="responsive-headline">Im Jonathan Doe.</h1>
                <h3>I'm a Manila based <span>graphic designer</span>, <span>illustrator</span> and <span>webdesigner</span> creating awesome and
-               effective visual identities for companies of all sizes around the globe. Lets <a classNameName="smoothscroll" href="#about">start scrolling</a>
+               effective visual identities for companies of all sizes around the globe. Lets <a className="smoothscroll" href="#about">start scrolling</a>
                and learn more <a className="smoothscroll" href="#about">about me</a>.</h3>
                <hr />
                <ul className="social">
